72bd18d96b082d05c6e964d5998924743ed04a08
Sean Maher <seanptmaher@gmail.com>
2020-09-02 22:15:25 +0000

    it's better now, (init-channel) is being run in my server

--- src/discfs.lisp
+++ src/discfs.lisp
@@ -25,7 +25,6 @@
  :on-message-create
  (lambda (msg) (format t "message ID: ~a~%" (lc:id msg)) (if (string= (lc:content msg) "discfs:ping") (reply msg "pong!"))))
 
-
 ;; In all the following: HASH is a sha-256 hash, ID is a 64-bit uint
 ;; represented as hex
 
@@ -45,13 +44,18 @@
 ;; NEXT {ID}
 ;; pointing to the next message in the chain
 
+;; format of files: base64!
+;; {BLOB}
+;; NEXT {id}
+;; the NEXT line is optional 
+
 (defun init-channel (channel-id)
   "this creates the jump tables. Takes forever. sorry."
   (let ((c (lispcord.http:from-id channel-id :channel)))
     (unless (= 0 (length (lispcord.http:get-pinned c)))
       (error "Channel already has pinned messages, did you mean to mount the channel?"))
-    (let ((v (make-array 64 :element-type integer))
-          (vouter (make-array 64 :element-type integer)))
+    (let ((v (make-array 64 :element-type 'integer))
+          (vouter (make-array 64 :element-type 'integer)))
       (dotimes (i 64)
         (dotimes (j 64)
           (setf (aref v j) (lc:id (lispcord.http:create "placeholder" c))))
@@ -67,20 +71,21 @@
 
 (defun mount-channel (channel-id)
   (setf *mntc* (lispcord.http:from-id channel-id :channel))
-  (assert (not (= 0 (length (lispcord.http:get-pinned c))))))
+  (assert (not (= 0 (length (lispcord.http:get-pinned *mntc*))))))
 
-(defun register (id path)
+(defun reg (id path)
   "this will add the hash to the filesystem in the given message. if
   the message is too full, go to NEXT message"
-  (lispcord.http:create))
+  (let ((msg (lispcord.http:from-id id :message)))
+    ()) )
 
-(defun upload (path)
+(defun put (path)
   "this will upload file into filesystem, and then return the hash at
   which this is stored, or NIL on error")
 
-(defun delete (hash)
+(defun del (hash)
   "this will delete file with given hash from filesystem, returning T
   on delete, NIL on no delete")
 
-(defun start (channel-id)
+(defun start ()
   (connect *discfs*))
