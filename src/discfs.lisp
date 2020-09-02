0ce68f22f5021e5d47562db149ce3066548b3e39
Sean Maher <seanptmaher@gmail.com>
2020-09-02 05:32:14 +0000

    bot committing time

--- src/discfs.lisp
+++ src/discfs.lisp
@@ -2,19 +2,25 @@
   (:use :cl :lispcord))
 
 (in-package :discfs)
+;; This is to load the token
+(load #p"secret.lisp")
+(if *token*
+    (format t "*token* is ~a~%" *token*)
+    (error "*token* not defined in src/secret.lisp"))
+
+(defbot *discfs* *token*)
+
+(add-event-handler
+ :on-ready
+ #'(lambda (ready)
+     (format t "User ~a~%Session ~a~%is ready~%"
+             (lc:name (lc:user ready))
+             (lc:session-id ready))))
+
+(add-event-handler
+ :on-message-create
+ (lambda (msg) (if (string= (lc:content msg) "ping!") (reply msg "pong!"))))
 
 
-
-(defvar *token* (sb-unix::posix-getenv "DISCFS-TOKEN"))
-
-(defbot *token*)
-
-
-(defun connect (&optional token)
-  (if *token*
-      (progn
-        
-        (connect *token*))
-      
-      (error "*token* not defined (or, DISCFS-TOKEN env var not in env)")))
-
+(defun start (channel-id)
+  (connect *discfs*))
